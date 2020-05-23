e89009455e57b8d6dabfdad90b904f7acb6b7458
Ankit Singh <ankitsingh@Ankits-MacBook-Pro.local>
2020-05-23 23:21:19 +0530

    RegistrationService template

--- service/src/main/java/services/JobSchedulerApp.java
+++ service/src/main/java/services/JobSchedulerApp.java
@@ -1,6 +1,10 @@
 package services;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import io.dropwizard.Application;
+import io.dropwizard.jersey.setup.JerseyEnvironment;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class JobSchedulerApp extends Application<JobSchedulerConfig> {
@@ -9,7 +13,14 @@ public class JobSchedulerApp extends Application<JobSchedulerConfig> {
         new JobSchedulerApp().run(args);
     }
 
+    public void initialize(Bootstrap<JobSchedulerConfig> bootstrap) {
+
+    }
+
     public void run(JobSchedulerConfig jobSchedulerConfig, Environment environment) throws Exception {
+        Injector injector = Guice.createInjector(new JobRegistrationModule());
+        final JerseyEnvironment jersey = environment.jersey();
+        jersey.register(injector.getInstance(JobRegistrationResource.class));
         System.out.println("Job scheduler app started");
     }
 }
--- service/src/main/java/services/JobSchedulerConfig.java
+++ service/src/main/java/services/JobSchedulerConfig.java
@@ -1,7 +1,11 @@
 package services;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 
 public class JobSchedulerConfig extends Configuration {
+
+    @JsonProperty("registeredClients")
+    private RegisteredClient clients;
 
 }
